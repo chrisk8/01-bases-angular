0be1e58c79c1978227f1a560a8132bf99519eda6
christian H <cchristian302@gmail.com>
2025-02-12 02:00:15 -0600

    Primera APP Counter antes de dividirla por componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root',  //Selector de referencia para crear la relacion entre el html y el .ts
+  templateUrl: './app.component.html',  //path del html
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css'  //path de los estilos
 })
-export class AppComponent {
-  title = '01-bases-angular';
+export class AppComponent {    //Propiedades de la clase
+  public title: string = '01-bases-angular';   
+  public counter: number = 10;
+
+
+  //Metodo para incrementar el contador
+  increaseByy(): void {
+    this.counter = this.counter + 1;  //Forma corta [this.counter += 1]
+  }
+
+  //Metodo para icrementar, decrementar el contador segun el argumento
+  increaseBy( value: number ): void {
+    this.counter += value;
+  }
+
+  //Metodo para resetear el contador
+  resetCounter(): void {
+    this.counter = 10;
+  }
+
 }
